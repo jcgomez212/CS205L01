638555eb2449e5e7036f39ee88ed47c009641616
jcgomez212 <91210626+jcgomez212@users.noreply.github.com>
2023-11-08 11:02:08 -0500

    DeckOfCards - Doubly Linked List Assignment Part 2

--- ListCardNode.java
+++ ListCardNode.java
@@ -1,81 +1,125 @@
+// modified wed nov 8 23'
+
 package deckOfCards;
 
 import edu.monmouth.card.*;
 
-private CardNode head;
-private CardNode tail;  // New tail attribute
+public class ListCardNode {
+    private CardNode head;
+    private CardNode tail; // New tail attribute
 
-public ListCardNode() {
-    head = tail = null;
-}
-
-public CardNode getTail() {
-    return tail;
-}
-
-public String printReverse() {
-    StringBuilder sb = new StringBuilder();
-    CardNode current = tail;
-    while (current != null) {
-        sb.append(current).append(", ");
-        current = current.getPrevious();
+    public ListCardNode() {
+        head = tail = null;
     }
-    // Removing the trailing comma and space
-    if (sb.length() > 0) {
-        sb.setLength(sb.length() - 2);
-    }
-    return sb.toString();
-}
 
-public void insertEnd(Card element) {
-    CardNode newNode = new CardNode(element);
-    if (tail == null) {
-        head = tail = newNode;
-    } else {
-        tail.setNext(newNode);
-        newNode.setPrevious(tail);
-        tail = newNode;
+    public CardNode getTail() {
+        return tail;
     }
-}
 
-public CardNode insertAfter(Card searchValue, Card insertValue) {
-    CardNode current = head;
-    while (current != null && !current.getCard().equals(searchValue)) {
-        current = current.getNext();
-    }
-    if (current != null) {
-        CardNode newNode = new CardNode(insertValue);
-        newNode.setNext(current.getNext());
-        newNode.setPrevious(current);
-        if (current.getNext() != null) {
-            current.getNext().setPrevious(newNode);
-        } else {
-            tail = newNode; // Update the tail if inserting at the end
+    public String printReverse() {
+        StringBuilder sb = new StringBuilder();
+        CardNode current = tail;
+        while (current != null) {
+            sb.append(current).append(", ");
+            current = current.getPrevious();
         }
-        current.setNext(newNode);
-        return newNode;
+        // Removing the trailing comma and space
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 2);
+        }
+        return sb.toString();
     }
-    return null;
-}
 
-public CardNode remove(Card searchValue) {
-    CardNode current = head;
-    while (current != null && !current.getCard().equals(searchValue)) {
-        current = current.getNext();
-    }
-    if (current != null) {
-        if (current.getPrevious() != null) {
-            current.getPrevious().setNext(current.getNext());
+    public void insertEnd(Card element) {
+        CardNode newNode = new CardNode(element);
+        if (tail == null) {
+            head = tail = newNode;
         } else {
-            head = current.getNext();  // Update the head if removing from the start
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail = newNode;
         }
-        if (current.getNext() != null) {
-            current.getNext().setPrevious(current.getPrevious());
-        } else {
-            tail = current.getPrevious();  // Update the tail if removing from the end
-        }
-        return current;
     }
-    return null;
-}
+
+    public CardNode insertAfter(Card searchValue, Card insertValue) {
+        CardNode current = head;
+        while (current != null && !current.getCard().equals(searchValue)) {
+            current = current.getNext();
+        }
+        if (current != null) {
+            CardNode newNode = new CardNode(insertValue);
+            newNode.setNext(current.getNext());
+            newNode.setPrevious(current);
+            if (current.getNext() != null) {
+                current.getNext().setPrevious(newNode);
+            } else {
+                tail = newNode; // Update the tail if inserting at the end
+            }
+            current.setNext(newNode);
+            return newNode;
+        }
+        return null;
+    }
+
+    public CardNode remove(Card searchValue) {
+        CardNode current = head;
+        while (current != null && !current.getCard().equals(searchValue)) {
+            current = current.getNext();
+        }
+        if (current != null) {
+            if (current.getPrevious() != null) {
+                current.getPrevious().setNext(current.getNext());
+            } else {
+                head = current.getNext(); // Update the head if removing from the start
+            }
+            if (current.getNext() != null) {
+                current.getNext().setPrevious(current.getPrevious());
+            } else {
+                tail = current.getPrevious(); // Update the tail if removing from the end
+            }
+            return current;
+        }
+        return null;
+    }
+
+    public int howMany(Card searchValue) {
+        CardNode current = head;
+        int count = 0;
+        while (current != null) {
+            if (current.getCard().equals(searchValue)) {
+                count++;
+            }
+            current = current.getNext();
+        }
+        return count;
+    }
+
+    public void uniqueInsert(Card insertValue) {
+        CardNode current = head;
+        boolean isUnique = true;
+        while (current != null) {
+            if (current.getCard().equals(insertValue)) {
+                isUnique = false;
+                break;
+            }
+            current = current.getNext();
+        }
+        if (isUnique) {
+            insertEnd(insertValue);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        CardNode current = head;
+        while (current != null) {
+            sb.append(current).append(", ");
+            current = current.getNext();
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 2);
+        }
+        return sb.toString();
+    }
 }
--- ListOfCards.java
+++ ListOfCards.java
@@ -1,46 +1,42 @@
+// modified wed nov 8 23'
+
 package edu.monmouth.cardList;
 
 import edu.monmouth.card.*;
+import deckOfCards.Card; // Import your Card class
 
 public class ListOfCards {
 
-	public static void main(String[] args) {
-		// verify constructor, toString and insert
-		ListCardNode fullList = new ListCardNode();
-		System.out.println("Before adding cards\nnumber of Cards: " + fullList.size());
-		System.out.println("isEmpty: " + fullList.isEmpty());
-		for (Rank rank : Rank.values()) { 
-			for (Suit suit : Suit.values()) { 
-				fullList.insert(new Card(rank, suit));
-			}
-		}
-		System.out.println(fullList);
-		System.out.println("After adding cards\nnumber of Cards: " + fullList.size());
-		System.out.println("isEmpty: " + fullList.isEmpty());
-		
-		// new list to verify new methods
-		ListCardNode list = new ListCardNode();
-		System.out.println("List in reverse: " + list.printReverse());
-		list.insert(new Card(Rank.ACE, Suit.CLUB));
-		list.insert(new Card(Rank.KING, Suit.CLUB));
-		list.insert(new Card(Rank.JACK, Suit.SPADE));
-		list.insert(new Card(Rank.ACE, Suit.HEART));
-		System.out.println("List after 4 cards: " + list);
-		System.out.println("List in reverse: " + list.printReverse());
-		
-		list.insertAfter(new Card(Rank.JACK, Suit.SPADE), new Card(Rank.THREE, Suit.HEART));
-		System.out.println("List after card added: " + list);
-	
-		list.insertEnd(new Card(Rank.FIVE, Suit.SPADE));
-		System.out.println("List after card added at end: " + list);
-		
-		Card cardToRemove = new Card(Rank.QUEEN, Suit.CLUB);
-		System.out.println(list.remove());
-		System.out.println("List after attempted remove of " + cardToRemove + "\n" + list);
+    public static void main(String[] args) {
+        ListCardNode fullList = new ListCardNode();
+        System.out.println("Before adding cards\nnumber of Cards: " + fullList.size());
+        System.out.println("isEmpty: " + fullList.isEmpty());
 
-		cardToRemove = new Card(Rank.ACE, Suit.CLUB);
-		System.out.println(list.remove());
-		System.out.println("List after attempted removal of " + cardToRemove + "\n" + list);		
-	}
+        for (Rank rank : Rank.values()) {
+            for (Suit suit : Suit.values()) {
+                fullList.insert(new Card(rank, suit));
+            }
+        }
+        System.out.println(fullList);
+        System.out.println("After adding cards\nnumber of Cards: " + fullList.size());
+        System.out.println("isEmpty: " + fullList.isEmpty());
 
+        ListCardNode list = new ListCardNode();
+        System.out.println("List in reverse: " + list.printReverse());
+        list.insert(new Card(Rank.ACE, Suit.CLUB));
+        list.insert(new Card(Rank.KING, Suit.CLUB));
+        list.insert(new Card(Rank.JACK, Suit.SPADE));
+        list.insert(new Card(Rank.ACE, Suit.HEART));
+        System.out.println("List after 4 cards: " + list);
+        System.out.println("List in reverse: " + list.printReverse());
+
+        Card cardToCount = new Card(Rank.ACE, Suit.CLUB);
+        int count = list.howMany(cardToCount);
+        System.out.println("Number of " + cardToCount + " in the list: " + count);
+
+        Card cardToInsert = new Card(Rank.THREE, Suit.HEART);
+        list.uniqueInsert(cardToInsert);
+        System.out.println("List after card added with uniqueInsert: " + list);
+        System.out.println("List in reverse: " + list.printReverse());
+    }
 }
